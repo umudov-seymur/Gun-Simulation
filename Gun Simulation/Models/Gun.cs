3a933bf001e3e48f22fb8cbf084ba5f39317cdf6
Seymur Umudov <u.seymur@outlook.com>
2021-11-03 12:26:27 +0400

    Initial commit

---
+++ Gun Simulation/Models/Gun.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun_Simulation.Models
+{
+    class Gun
+    {
+        public string Type { get; set; }
+
+        private int _totalBulletSize;
+        public int TotalBulletSize
+        {
+            get { return _totalBulletSize; }
+            private set
+            {
+                if (value >= 0)
+                {
+                    _totalBulletSize = value;
+                }
+            }
+        }
+
+        private int _magazineSize;
+        public int MagazineSize
+        {
+            get { return _magazineSize; }
+            set
+            {
+                if (value > 0)
+                {
+                    _magazineSize = value;
+                }
+            }
+        }
+
+        private int _bulletLeft;
+        public int BulletLeft
+        {
+            get { return _bulletLeft; }
+            set
+            {
+                if (_magazineSize >= value)
+                {
+                    _bulletLeft = value;
+                }
+            }
+        }
+
+        public Gun(string type, int totalBulletSize, int magazineSize)
+        {
+            Type = type;
+            TotalBulletSize = totalBulletSize;
+            MagazineSize = magazineSize;
+            BulletLeft = MagazineSize;
+        }
+
+        public string Shoot(int fireBulletCount)
+        {
+            if (fireBulletCount > BulletLeft)
+            {
+                return $"\nThe number of bullets you fire must be less than {BulletLeft + 1}.";
+            }
+
+            BulletLeft -= fireBulletCount;
+
+            return $"\nYou fired {fireBulletCount} bullets.";
+        }
+
+        public string Shoot()
+        {
+            int remainingBullet = BulletLeft;
+            BulletLeft -= remainingBullet;
+            return $"\nYou fired {remainingBullet} bullets.";
+        }
+
+        public string Reload(int bulletCount)
+        {
+            int bulletNeedCount = MagazineSize - BulletLeft;
+
+            if (bulletNeedCount >= bulletCount)
+            {
+                TotalBulletSize -= bulletCount;
+                BulletLeft += bulletCount;
+                return $"\nYour gun is reloaded with {bulletCount} bullet.";
+            }
+
+            return $"\nAccepted bullet count: {MagazineSize}";
+        }
+
+        public string Reload()
+        {
+            if (BulletLeft < MagazineSize)
+            {
+                int bulletNeedCount = MagazineSize - BulletLeft;
+                TotalBulletSize -= bulletNeedCount;
+                BulletLeft = MagazineSize;
+                return $"\nYour gun is reloaded with {bulletNeedCount} bullet.";
+            }
+            return $"\nYou can't, because gun magazine is full.";
+        }
+
+        public override string ToString()
+        {
+            return $"\nGun type is: {Type}\n" +
+                    $"Total bullet: {TotalBulletSize}\n" +
+                    $"Bullet remaining count: {BulletLeft}\n";
+        }
+    }
+}
---
+++ Gun Simulation/Program.cs
@@ -0,0 +1,63 @@
+﻿using Gun_Simulation.Models;
+using System;
+
+namespace Gun_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun("Glock", 100, 30);
+
+            while (true)
+            {
+                try
+                {
+                    GunMenu(gun);
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid!\nTry Again!");
+                }
+            }
+        }
+
+        static void GunMenu(Gun gun)
+        {
+            Console.WriteLine("Please Choose\n1 - Reload Gun\n2 - Shoot\n3 - Full Shoot\n4 - Gun information\n");
+
+            int choicedActionNumber = int.Parse(Console.ReadLine());
+
+            switch (choicedActionNumber)
+            {
+                case 1:
+                    Console.Write("Fully reload or manual? Y/N: ");
+                    string isFullReload = Console.ReadLine();
+
+                    if (isFullReload.ToLower() == "y")
+                    {
+                        Console.WriteLine(gun.Reload());
+                    }
+                    else
+                    {
+                        Console.Write("Enter bullet count: ");
+                        Console.WriteLine(gun.Reload(int.Parse(Console.ReadLine())));
+                    }
+
+                    break;
+                case 2:
+                    Console.Write("How many bullets do you shoot ? ");
+                    int bulletCount = int.Parse(Console.ReadLine());
+                    Console.WriteLine(gun.Shoot(bulletCount));
+                    break;
+                case 3:
+                    Console.WriteLine(gun.Shoot());
+                    break;
+                case 4:
+                    Console.WriteLine(gun.ToString());
+                    break;
+            }
+        }
+    }
+}
